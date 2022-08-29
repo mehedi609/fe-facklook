c2a5fae5346661c29c20e92fd0a50d642d887cda
mehediPC <mehedi609@gmail.com>
2022-08-29 20:50:40 +0600

    Implemented react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div>welcome to frontend</div>;
-}
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Profile from './pages/profile';
+import Home from './pages/home';
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
