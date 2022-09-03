ace0627ed125c47d445e284ccb101b5e5a36f772
mehediPC <mehedi609@gmail.com>
2022-09-03 16:52:31 +0600

    Implemented right sidebar

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,7 @@
 import Header from '../../components/header';
 import { useSelector } from 'react-redux';
 import LeftHome from '../../components/home/left';
+import RightHome from '../../components/home/right';
 
 export default function Home() {
   const {
@@ -11,6 +12,7 @@ export default function Home() {
     <div>
       <Header />
       <LeftHome user={user} />
+      <RightHome user={user} />
     </div>
   );
 }
