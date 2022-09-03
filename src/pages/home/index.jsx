0a897474e56a678ca29abfb07e0192e218a292c6
mehediPC <mehedi609@gmail.com>
2022-09-03 16:20:11 +0600

    Implemented left sidebar

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,16 @@
 import Header from '../../components/header';
+import { useSelector } from 'react-redux';
+import LeftHome from '../../components/home/left';
 
 export default function Home() {
+  const {
+    auth: { loggedInUser: user },
+  } = useSelector((state) => state);
+
   return (
     <div>
       <Header />
+      <LeftHome user={user} />
     </div>
   );
 }
